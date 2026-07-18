f5434805354da27487abffeacc28ab242d2fec3c
shargaiov <sshargaev2008@gmail.com>
2026-07-18 15:48:31 +0600

    feat: add FileSystemCommands

---
+++ FileSystemCommands/DirectorySizeCommand.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Linq;
+using CommandLib;
+
+namespace FileSystemCommands;
+
+public class DirectorySizeCommand : ICommand
+{
+    private readonly string _targetPath;
+    
+    public long CalculatedSize { get; private set; }
+
+    public DirectorySizeCommand(string targetPath)
+    {
+        _targetPath = targetPath;
+    }
+
+    public void Execute()
+    {
+        CalculatedSize = 0;
+
+        if (Directory.Exists(_targetPath))
+        {
+            CalculatedSize = new DirectoryInfo(_targetPath)
+                .GetFiles("*", SearchOption.AllDirectories)
+                .Sum(file => file.Length);
+        }
+    }
+}
